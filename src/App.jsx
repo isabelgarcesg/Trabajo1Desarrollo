31265a72076f79f571d1935cf7fd358c5265ff3a
isabelgarcesg <65743747+isabelgarcesg@users.noreply.github.com>
2023-10-19 19:08:41 -0500

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,29 @@ const registros = [
     "_id": 1,
     "paraclinicos": "Frecuencia cardiaca: 80 bpm, Frecuencia respiratoria: 18 rpm, Temperatura: 37.2°C",
     "evolucion": "El paciente se encuentra estable. Continuar con el tratamiento actual.",
-    "procedimiento_id": 1,
-    "medicamento_id": [
-      "2, 4, 8"
-    ],
-    "evento_id": 17,
-    "personal_id": "4826109375",
+    "procedimiento": {
+      "_id": 1,
+      "nombre": "Extracción de sangre",
+      "descripcion": "Proceso para recoger una muestra de sangre para análisis médicos."
+    },
+    "medicamento": 
+      {
+        "_id": 2,
+        "nombre": "Ibuprofeno",
+        "dosis": "400 mg",
+        "via": "Oral",
+        "frecuencia_dia": 3,
+        "duracion_dias": 5,
+        "observaciones": "No exceder la dosis recomendada"
+      },
+    "personal": {
+      "_id": 10,
+      "tipo_id": "cedula",
+      "nombre": "Pedro Jesús Giraldo",
+      "email": "jesusg@gmail.com",
+      "doc_identidad": "4826109375",
+      "cargo": "Enfermero"
+    },
     "especialidad": "Enfermería",
     "fecha": {
       "$date": "2022-06-22T05:00:00.000Z"
@@ -21,12 +38,29 @@ const registros = [
     "_id": 2,
     "paraclinicos": "Frecuencia cardiaca: 75 bpm, Frecuencia respiratoria: 20 rpm, Temperatura: 36.8°C",
     "evolucion": "El paciente presenta mejoría en los síntomas. Seguir monitoreando.",
-    "procedimiento_id": 2,
-    "medicamento_id": [
-      "10"
-    ],
-    "evento_id": 1,
-    "personal_id": "29876543210",
+    "procedimiento": {
+  "_id": 2,
+  "nombre": "Radiografía de tórax",
+  "descripcion": "Obtención de imágenes de los pulmones y el corazón mediante radiación."
+},
+    "medicamento": 
+      {
+        "_id": 10,
+        "nombre": "Diazepam",
+        "dosis": "5 mg",
+        "via": "Oral",
+        "frecuencia_dia": 3,
+        "duracion_dias": 14,
+        "observaciones": "No beber alcohol mientras se toma"
+      },
+    "personal": {
+      "_id": 2,
+      "tipo_id": "cedula",
+      "nombre": "Paulina González",
+      "email": "mpaulina@gmail.com",
+      "doc_identidad": "9876543210",
+      "cargo": "Enfermera"
+    },
     "especialidad": "Enfermería",
     "fecha": {
       "$date": "2023-10-03T05:00:00.000Z"
@@ -36,12 +70,28 @@ const registros = [
     "_id": 3,
     "paraclinicos": "Ecografía abdominal sin novedades.",
     "evolucion": "Se ha iniciado un nuevo tratamiento. Paciente en observación.",
-    "procedimiento_id": 3,
-    "medicamento_id": [
-      "2"
-    ],
-    "evento_id": 2,
-    "personal_id": "2468135790",
+    "procedimiento": {
+      "_id": 3,
+      "nombre": "Ecografía abdominal",
+      "descripcion": "Uso de ondas de sonido para visualizar los órganos internos del abdomen."
+    },
+    "medicamento":{
+      "_id": 2,
+      "nombre": "Ibuprofeno",
+      "dosis": "400 mg",
+      "via": "Oral",
+      "frecuencia_dia": 3,
+      "duracion_dias": 5,
+      "observaciones": "No exceder la dosis recomendada"
+    },
+    "personal": {
+      "_id": 3,
+      "tipo_id": "cedula",
+      "nombre": "Francisco Arango",
+      "email": "fa1990@hotmail.com",
+      "doc_identidad": "2468135790",
+      "cargo": "Medico"
+    },
     "especialidad": "Gastroenterología",
     "fecha": {
       "$date": "2023-09-17T05:00:00.000Z"
--- src/components/Registros.jsx
+++ src/components/Registros.jsx
@@ -2,41 +2,54 @@ import { ChatBubbleOvalLeftIcon, ArrowPathRoundedSquareIcon } from '@heroicons/r
 
 function Registro({ registro }) {
     return (
-        <div className="flex gap-4 p-4 items-start">
+        <div className="flex gap-4 p-4 items-center justify-center w-1000">
             {/* <img className="rounded-full" src={registro.user.profileImage} alt={registro.user.name} width="40" /> */}
-            <div>
-                <a>{registro.paraclinicos}</a>
-                <a>{registro.paraclinicos}</a>
-                <a>{registro.paraclinicos}</a>
-                <a>{registro.paraclinicos}</a>
-                <a>{registro.paraclinicos}</a>
-                <a>{registro.paraclinicos}</a>
-                {/* <a className="mr-2 text-black" href="#">{registro.user.username}</a>
-                <span className="text-gray-600">· {registro.creationDate}</span>
-
-                <p className="mt-4">{registro.text}</p>
-
-                <div className='mt-4 flex justify-between'>
-                    <button className='text-gray-600 flex gap-1 items-center text-xs hover:text-sky-500'>
-                        <ChatBubbleOvalLeftIcon className="h-5 w-5 text-gray" />
-                        <span>{registro.replays}</span>
-                    </button>
-                    <button className='text-gray-600 flex gap-1 items-center text-xs hover:text-green-500'>
-                        <ArrowPathRoundedSquareIcon className="h-5 w-5 text-gray" />
-                        <span>{registro.reregistros}</span>
-                    </button>
-                    <button className='text-gray-600 flex gap-1 items-center text-xs hover:text-green-500'>
-                        <ArrowPathRoundedSquareIcon className="h-5 w-5 text-gray" />
-                        <span>20</span>
-                    </button>
-                    <button className='text-gray-600 flex gap-1 items-center text-xs hover:text-green-500'>
-                        <ArrowPathRoundedSquareIcon className="h-5 w-5 text-gray" />
-                        <span>20</span>
-                    </button> */}
-                {/* </div> */}
+            <div style={divStyle}>
+                <p className="font-bold" >paraclinicos</p>
+                <p>{registro.paraclinicos}</p>
+                <p className="font-bold" >Evolucion</p>
+                <p>{registro.evolucion}</p>
+                <p className="font-bold" >Procedimientos</p>
+                <p className="font-bold" >Nombre</p>
+                <p>{registro.procedimiento.nombre}</p>
+                <p className="font-bold" >Descripcion</p>
+                <p>{registro.procedimiento.descripcion}</p>
+                <p className="font-bold" >Medicamentos</p>
+                <p className="font-bold" >Nombre</p>
+                <p>{registro.medicamento.nombre}</p>
+                <p className="font-bold" >Dosis</p>
+                <p>{registro.medicamento.dosis}</p>
+                <p className="font-bold" >Administracion via</p>
+                <p>{registro.medicamento.via}</p>
+                <p className="font-bold" >Frecuencia</p>
+                <p>{registro.medicamento.frecuencia_dia}</p>
+                <p className="font-bold" >Duracion del tratamiento</p>
+                <p>{registro.medicamento.duracion_dias}</p>
+                <p className="font-bold" >Observaciones</p>
+                <p>{registro.medicamento.observaciones}</p>
+                <p className="font-bold" >Personal Asistencial</p>
+                <p className="font-bold" >Nombre</p>
+                <p>{registro.personal.nombre}</p>
+                <p className="font-bold" >Correo</p>
+                <p>{registro.personal.email}</p>
+                <p className="font-bold" >Doc de identidad</p>
+                <p>{registro.personal.doc_identidad}</p>
+                <p className="font-bold" >Cargo</p>
+                <p>{registro.personal.cargo}</p>
+                <p className="font-bold" >Especialidad</p>
+                <p>{registro.especialidad}</p>
+                <p className="font-bold" >Fecha</p>
+                <p>{registro.fecha.$date}</p>
             </div>
         </div>
     );
 }
 
 export default Registro;
+
+const divStyle = {
+    backgroundColor: 'white', // Cambia esto al color de fondo que desees
+    margin: '10px',
+    padding: '10px', // Añadí padding para que el margen interior tenga espacio
+  };
+
